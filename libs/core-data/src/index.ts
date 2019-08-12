489b2f7a037edb7ddc9c454e7c417c8465a48da6
eccentricnode <shival.bigman@gmail.com>
2019-08-11 22:38:08 -0700

    setup: auth in app component; setup: layout and styling in app.component

--- libs/core-data/src/index.ts
+++ libs/core-data/src/index.ts
@@ -1,3 +1,5 @@
 export { CoreDataModule } from './lib/core-data.module';
+export { AuthService } from './lib/auth/auth.service';
+
 export { AnimalsService } from './lib/animals/animals.service';
 export { Animal, emptyAnimal } from './lib/animals/animal.model';
