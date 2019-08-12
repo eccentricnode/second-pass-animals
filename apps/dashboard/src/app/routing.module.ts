7e2bbdcbaae980741779baf13658091b7512d943
eccentricnode <shival.bigman@gmail.com>
2019-08-11 22:07:58 -0700

    incremental checkin

--- apps/dashboard/src/app/routing.module.ts
+++ apps/dashboard/src/app/routing.module.ts
@@ -3,4 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component'
 import { AnimalsComponent } from './animals/animals.component';
 
+const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'animals', component: AnimalsComponent },
+    { path: '**', redirectTo: '/login', pathMatch: 'full' },
+];
 
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class RouteingModule {}
