4acfe19edbdf47ff226065133662668c2df7e5a8
eccentricnode <shival.bigman@gmail.com>
2019-08-12 19:24:56 -0700

    incremental checkin

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NxModule } from '@nrwl/angular';
 import { MaterialModule } from '@animals/material';
@@ -23,6 +24,7 @@ import { RoutingModule } from './routing.module';
     NxModule.forRoot(),
     CoreDataModule,
     CoreStateModule,
+    HttpClientModule,
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
